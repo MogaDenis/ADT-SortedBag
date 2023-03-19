96c81f3e73e7b9124f003cf3117bf8d06209030f
Moga Denis <moga_denis@yahoo.com>
2023-03-19 14:31:37 +0200

    .

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ -Wall -g "Source files/App.cpp" "Source files/ExtendedTest.cpp" "Source files/ShortTest.cpp" "Source files/SortedBag.cpp" "Source files/SortedBagIterator.cpp" -o main
